19cce27287a976eef96e520984eea725d1873269
Your Name <you@example.com>
2021-08-26 09:29:57 +0700

    javascript

---
+++ h.js
@@ -0,0 +1,33 @@
+var fullName = 'Dang ngọc';//khai báo biến
+var age = 26;
+
+alert(fullName);//gọi hàm thông báo
+alert(age);
+/**
+ * giới thiệu một số hàm built -in
+ * 1. Alert
+ * 2. Console
+ * 3.comfirm
+ * 4.prompt
+ * 5. set timeout
+ * 6. set intreval
+ */
+var fullname = "Dang ngọc sơn";
+console.log(fullname);
+confirm('Xác nhận đủ 18 tuổi');
+prompt("xác nhận bạn đủ tuổi");
+setTimeout(function(){
+ alert("thong bao")
+},3000)
+setInterval(function ()  {
+    console.log('hay den ben an' + Math.random)
+},1000)
+
+/**
+ * 1. toán tử số học arthmetic
+ * 2. toán tử gán- Assignment
+ * 3. toán tử so sánh - comparsion
+ * 4. toán tử logic - logical
+ */
+
+
---
+++ hu.js
@@ -0,0 +1,4 @@
+var a=3;
+var b=5;
+c=a+b;
+alert(c);
